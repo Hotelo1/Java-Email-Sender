45cdc557ed03f30f4ac2c907501a8091fb57eb66
Hotelo1 <cardadort@gmail.com>
2023-09-15 13:14:47 -0300

    withsource_clean

---
+++ src/main/java/com/hotelo/EmailService/application/EmailSenderService.java
@@ -0,0 +1,21 @@
+package com.hotelo.EmailService.application;
+
+import com.hotelo.EmailService.adapters.EmailSenderGateway;
+import com.hotelo.EmailService.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    @Autowired
+    public EmailSenderService(EmailSenderGateway emailGateway){
+        this.emailSenderGateway = emailGateway;
+    }
+
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        this.emailSenderGateway.sendEmail(to, subject, body);
+
+    }
+}
